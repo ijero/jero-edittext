328d26f92e35887632c375cf4f58a9acd6ffe599
Jero <ijero@qq.com>
2017-12-14 15:56:43 +0800

    完成单行编辑框

---
+++ library/src/main/java/cn/ijero/edittext/JeroMultiLineEditText.kt
@@ -0,0 +1,12 @@
+package cn.ijero.edittext
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.RelativeLayout
+
+/**
+ * @author Jero . Created on 2017/12/14.
+ */
+class JeroMultiLineEditText
+@JvmOverloads constructor(ctx: Context, attrs: AttributeSet? = null) : RelativeLayout(ctx, attrs) {
+}
